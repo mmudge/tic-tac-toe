aa487e2463c6aa78c8dc530bc19166b60d1308f2
michael mudge <michaeltmudge@gmail.com>
2021-01-24 18:52:15 -0800

    form and board in a flex container

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ import { header } from './components/header.js'
 import { playerForm } from './components/playerForm.js'
 import { board } from './components/board.js'
 import { playerInfo } from './components/playerInfo.js'
+import { createElementFromHTML } from './helpers/createElementFromHTML.js'
 
 const app = document.getElementById('app')
 app.insertAdjacentElement('afterbegin', appWrapper())
 
 const container = document.querySelector('.container')
 
-container.insertAdjacentElement('afterbegin', header())
-container.insertAdjacentElement('beforeend', playerForm())
+const headerEl = header()
+
+container.insertAdjacentElement('afterbegin', headerEl)
+
+const content = createElementFromHTML('<div class="content"></div>')
+
+container.insertAdjacentElement('beforeend', content)
+
+const leftContent = createElementFromHTML('<div class="left-content"></div>')
+
+content.insertAdjacentElement('beforeend', leftContent)
+
+leftContent.insertAdjacentElement('beforeend', playerForm())
 
 const button = document.querySelector('button')
 button.addEventListener('click', (event) => {
@@ -24,7 +36,8 @@ button.addEventListener('click', (event) => {
   const playerTwo = document.getElementById('player-2-name').value
 
   const formContainer = document.querySelector('.form-container')
-  formContainer.insertAdjacentElement('afterend', playerInfo(playerOne, playerTwo))
+  // formContainer.insertAdjacentElement('afterend', playerInfo(playerOne, playerTwo))
+  headerEl.insertAdjacentElement('beforeend', playerInfo(playerOne, playerTwo))
 
   const inputs = document.querySelectorAll('input')
   for (let i = 0; i < inputs.length; i++) {
@@ -32,4 +45,8 @@ button.addEventListener('click', (event) => {
   }
 })
 
-container.insertAdjacentElement('beforeend', board())
+const rightContent = createElementFromHTML('<div class="right-content"></div>')
+
+content.insertAdjacentElement('beforeend', rightContent)
+
+rightContent.insertAdjacentElement('beforeend', board())
